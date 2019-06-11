3183dac351d134f608eb48376c542db632a746d4
Piotr Walat <pwalat98@gmail.com>
2019-06-11 22:00:27 +0200

    commit - v0.01
    Done:
    *basic movement
    *camera following player
    *basic (non-interactable) main menu scene
    WIP:
    *player rotation toward mouse position
    *bullet firing in rotation direction

---
+++ Warhammer40k_Game/Assets/Scripts/Player_scripts/Player.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector3 my_forward_position_angle;
+
+
+    private float speed = 10f;
+    private float rotation_speed = 5f;
+    
+
+    private Rigidbody2D rb_player;
+    private Vector2 moveVelocity;
+    private Vector2 dashmoveVelocity;
+
+
+    private Vector3 lastMoveDir;
+
+    private float x_compound;
+    private float y_compound;
+
+   
+    void Start()
+    {
+        rb_player = GetComponent<Rigidbody2D>();
+        x_compound = rb_player.position.x;
+        y_compound = rb_player.position.y;
+    }
+
+    void Update()
+    {
+
+        HandleMovement();
+        
+        HandleDash();
+    }
+
+    
+
+
+    
+
+
+    private void HandleMovement()
+    {
+        float moveX = 0f;
+        float moveY = 0f;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveY = +1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveY = -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX = +1f;
+        }
+
+        bool isIdle = moveX == 0 && moveY == 0;
+        if (!isIdle)
+        {
+            Vector3 moveDir = new Vector3(moveX, moveY).normalized;
+            lastMoveDir = moveDir;
+            transform.position += moveDir * speed * Time.deltaTime;
+        }
+        
+    }
+
+    private void HandleDash()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float dashDistance = 5f;
+            transform.position += lastMoveDir * dashDistance;
+        }
+    }
+
+    private bool CanMove(Vector3 dir, float distance)
+    {
+        return Physics2D.Raycast(transform.position, dir, distance).collider == null;
+    }
+}
